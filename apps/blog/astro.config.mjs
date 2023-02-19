0ccb1decdb159f51ca77576f9cb7bde6b6163195
Balint Orosz <orosz.balint.97@gmail.com>
2023-02-19 11:12:13 +0100

    Initial commit

---
+++ apps/blog/astro.config.mjs
@@ -0,0 +1,10 @@
+import { defineConfig } from 'astro/config';
+import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
+
+export default defineConfig({
+	site: 'https://example.com',
+	integrations: [mdx(), sitemap()],
+  outDir: '../../dist/apps/blog',
+});
+
